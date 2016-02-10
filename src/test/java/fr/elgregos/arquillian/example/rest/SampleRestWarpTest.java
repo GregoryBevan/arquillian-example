3847c6ca664fb5117ed58f4984ec104d11a0dd0c
Gregory Bevan <gregory.bevan@vif.fr>
2016-02-10 08:26:19 +0100

    Add dependencies for arquillian Warp REST test

---
+++ src/test/java/fr/elgregos/arquillian/example/rest/SampleRestWarpTest.java
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+package fr.elgregos.arquillian.example.rest;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.warp.WarpTest;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+@WarpTest
+public class SampleRestWarpTest {
+
+}
